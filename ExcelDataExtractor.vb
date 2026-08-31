184757618e332121c0893c3c218dbfaa0986eebe
AlvaroPersichini <alvaro.persichini@gmail.com>
2026-08-31 09:42:14 -0300

    Update

--- ExcelDataExtractor.vb
+++ ExcelDataExtractor.vb
@@ -47,38 +47,3 @@ Public Class ExcelDataExtractor
         End Try
     End Function
 End Class
-
-
-
-
-
-
-
-
-
-
-
-'Function ExtractData(oSheet As Microsoft.Office.Interop.Excel.Worksheet) As Dictionary(Of String, ExcelData)
-'    Dim lastRow As Integer = GetLastRow(oSheet)
-'    ' Si la hoja no tiene datos después del encabezado, salimos temprano
-'    ' CUIDADO esto hay que manejarlo de otra manera.
-'    Dim oDic As New Dictionary(Of String, ExcelData)
-'    If lastRow < 3 Then Return oDic
-'    For i As Integer = 3 To lastRow
-'        Dim key As String = oSheet.Cells(i, 4).Text.ToString()
-'        ' Validamos que la clave no esté vacía antes de agregar
-'        If Not String.IsNullOrWhiteSpace(key) AndAlso Not oDic.ContainsKey(key) Then
-'            Dim oExcelData As New ExcelData With {
-'                .NewPartNumber = oSheet.Cells(i, 5).Text.ToString(),  ' E
-'                .DescriptionRef = oSheet.Cells(i, 6).Text.ToString(), ' F
-'                .Quantity = oSheet.Cells(i, 7).Text.ToString(),       ' G
-'                .Source = oSheet.Cells(i, 8).Text.ToString(),         ' H
-'                .Nomenclature = oSheet.Cells(i, 10).Text.ToString(),  ' J
-'                .Definition = oSheet.Cells(i, 11).Text.ToString()     ' K
-'            }
-'            oDic.Add(key, oExcelData)
-'        End If
-'    Next
-'    Return oDic
-'End Function
-
